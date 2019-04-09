a9907f7b5d88c9b1a2d9357e69956397ba37325a
stan wt <stanislas.wiart@gmail.com>
2019-04-09 23:23:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('#search-movies');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // <-- to prevent <form>'s native behaviour
+  const input = event.currentTarget.querySelector('.form-control');
+  results.innerHTML = '';
+  searchMovies(input.value);
+});
+
+
+
+const searchMovies = (query) => {
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        const movie = `<li>
+          <img src="${result.Poster}" alt="">
+          <p>${result.Title}</p>
+        </li>`;
+        results.insertAdjacentHTML("beforeend", movie);
+      });
+    });
+};
